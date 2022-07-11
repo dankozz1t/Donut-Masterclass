599f4ef4a87c6307aff4fc7c0c756e3d721a5ad7
Oleksandr Kholod <avxolod@gmail.com>
2022-07-11 16:15:11 +0300

    add wrapper

---
+++ src/js/slick.js
@@ -0,0 +1,29 @@
+$('.reviews__list').slick({
+  // normal options...
+  infinite: true,
+  arrows: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+
+  // the magic
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 300,
+      settings: 'unslick', // destroys slick
+    },
+  ],
+});
